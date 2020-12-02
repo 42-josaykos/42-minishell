56aa4ae35da6528ae590302b005851b7419ff3e6
Jonny Saykosy <contact@josayko.com>
2020-12-02 14:31:06 +0100

    replace msh_loop with new function get_input

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jonny <jonny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 11:51:53 by jonny             #+#    #+#             */
-/*   Updated: 2020/11/30 13:25:30 by jonny            ###   ########.fr       */
+/*   Updated: 2020/12/02 14:27:21 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,42 @@
 #include <stdlib.h>
 #include "../libft/libft.h"
 
-void msh_loop(void)
+#define MAXCHAR 50
+
+void init_msh(void)
+{
+	ft_printf("Welcome to minishell (msh)!\nCtrl-C or \"exit\" to quit msh.\n");
+}
+
+int get_input(char *input)
 {
 	char *line;
 
 	line = NULL;
-	ft_printf("Welcome to minishell (msh)!\nCtrl-C or \"exit\" to quit msh.\n");
-	while (1)
-	{
-		ft_printf("$ ");
-		get_next_line(0, &line);
-		if (ft_strncmp(line, "exit", 4) == 0)
-			break ;
-		ft_printf("%s\n", line);
-		free(line);
-		line = NULL;
-	}
+	ft_printf("$ ");
+	get_next_line(0, &line);
+	ft_strlcpy(input, line, MAXCHAR);
 	free(line);
-	line = NULL;
+	return (0);
 }
 
 int main(int argc, char **argv)
 {
 	(void)argv;
+	char input[MAXCHAR];
+
 	if (argc < 2)
-		msh_loop();
+	{
+		init_msh();
+		while (1)
+		{
+			get_input(input);
+			if (ft_strncmp(input, "exit", 4) == 0)
+				break ;
+			ft_printf("%s\n", input);
+		}
+	}
+	else
+		printf("Usage: just ./minishell with no arguments.\n");
 	return EXIT_SUCCESS;
 }
