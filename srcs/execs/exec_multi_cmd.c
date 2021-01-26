fac02868b4e854a5910a29dd59448dc457c332cf
Jonny Saykosy <josaykos@student.42.fr>
2021-01-26 16:50:25 +0100

    BIG UPDATE: Add multicommands feature (with semicolon ;)

--- srcs/execs/exec_multi_cmd.c
+++ srcs/execs/exec_multi_cmd.c
@@ -6,30 +6,39 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 15:21:45 by jonny             #+#    #+#             */
-/*   Updated: 2021/01/26 15:50:58 by jonny            ###   ########.fr       */
+/*   Updated: 2021/01/26 16:47:29 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/msh.h"
+#include <sys/wait.h>
 
-static void	exec_multi_cmd(char **envp, int n, t_cmd *cmd_lst)
+static void	exec_multi_cmd(char **envp, t_env *env_lst, int n, t_cmd *cmd_lst)
 {
 	pid_t	pid;
+	int		ret;
+	int		status;
 
-	if (create_fork(&pid) < 0 )
-		exit(-1);
 	while (n > 0)
 	{
-		if (pid == 0)
+		ret = is_builtin(*cmd_lst->args);
+		if (!file_exists(*cmd_lst->args))
+			cmd_lst->args[0] = cmd_lst->cmd;
+		if (ret)
+			exec_builtin(ret, env_lst, cmd_lst);
+		else
 		{
-			if (!file_exists(*cmd_lst->args))
-				cmd_lst->args[0] = cmd_lst->cmd;
-			execve(*cmd_lst->args, cmd_lst->args, envp);
-			exit(0);
+			if (create_fork(&pid) < 0 )
+				exit(-1);
+			if (pid == 0)
+			{
+				execve(*cmd_lst->args, cmd_lst->args, envp);
+				exit(status);
+			}
 		}
-		n--;
 		cmd_lst = cmd_lst->next;
-		wait(NULL);
+		n--;
+		waitpid(pid, &status, WCONTINUED);
 	}
 }
 
@@ -55,16 +64,16 @@ static void	multi_cmd_handler3(t_cmd *cmd_lst, char *filepath)
 	}
 }
 
-static void	multi_cmd_handler2(char **envp, char *path, t_cmd *cmd_lst)
+void	multi_cmd_handler2(char **envp, t_env *env_lst, char *s, t_cmd *cmd_lst)
 {
 	char	filepath[MAXCHAR];
 	char	*tmp;
 	int		len;
 
 	len = 0;
-	while (path && cmd_lst)
+	while (s && cmd_lst)
 	{
-		tmp = ft_strsep(&path, ":");
+		tmp = ft_strsep(&s, ":");
 		len = ft_strlen(tmp);
 		ft_strlcpy(filepath, tmp, len + 1);
 		if (filepath[len - 1] != '/')
@@ -72,7 +81,7 @@ static void	multi_cmd_handler2(char **envp, char *path, t_cmd *cmd_lst)
 		multi_cmd_handler3(cmd_lst, filepath);
 	}
 	len = cmd_lst_size(cmd_lst);
-	exec_multi_cmd(envp, len, cmd_lst);
+	exec_multi_cmd(envp, env_lst, len, cmd_lst);
 }
 
 void 	multi_cmd_handler(char **envp, t_env *env_lst, t_cmd *cmd_lst)
@@ -91,5 +100,5 @@ void 	multi_cmd_handler(char **envp, t_env *env_lst, t_cmd *cmd_lst)
 		}
 		env_lst = env_lst->next;
 	}
-	multi_cmd_handler2(envp, pathstr, cmd_lst);
+	multi_cmd_handler2(envp, env_lst, pathstr, cmd_lst);
 }
