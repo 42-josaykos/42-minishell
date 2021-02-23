1001cb29477af0b18310503d8d4c7825c74b716c
Jonny Saykosy <josayko@pm.me>
2021-02-23 11:32:54 +0100

    update builtin exit with exit codes

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,13 +6,13 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 11:47:40 by jonny             #+#    #+#             */
-/*   Updated: 2021/02/21 23:10:51 by jonny            ###   ########.fr       */
+/*   Updated: 2021/02/23 11:31:12 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/msh.h"
 
-bool	ft_isnum(char *str)
+static bool	ft_isnum(char *str)
 {
 	while (*str)
 	{
@@ -23,27 +23,10 @@ bool	ft_isnum(char *str)
 	return (true);
 }
 
-int 	exit_msh(t_state *status, t_env *env_lst, t_cmd *cmd_lst)
+static void	free_exit(t_state *status, t_env *env_lst, t_cmd *cmd_lst)
 {
-	int		code;
 	t_cmd	*tmp;
 
-	code = 0;
-	ft_putstr_fd("exit\n", STDERR);
-	// if (cmd_lst->args && cmd_lst->args[1] && !ft_isnum(cmd_lst->args[1]))
-	// {
-	// 	code = 255;
-	// 	ft_putstr_fd("minishell: exit: ", STDERR);
-	// 	ft_putstr_fd(cmd_lst->args[1], STDERR);
-	// 	ft_putendl_fd(": numeric argument required", STDERR);
-	// }
-	// else if (cmd_lst->args && cmd_lst->args[1] && cmd_lst->args[2])
-	// {
-	// 	code = 1;
-	// 	ft_putstr_fd("minishell: exit: too many arguments\n", STDERR);
-	// }
-	// else if (cmd_lst->args && cmd_lst->args[1])
-	// 	code = ft_atoi(cmd_lst->args[1]);
 	if (status->envp)
 		free_2darray(status->envp);
 	free(status);
@@ -55,6 +38,29 @@ int 	exit_msh(t_state *status, t_env *env_lst, t_cmd *cmd_lst)
 		free(cmd_lst);
 		cmd_lst = tmp;
 	}
+}
+
+int 	exit_msh(t_state *status, t_env *env_lst, t_cmd *cmd_lst)
+{
+	int		code;
+
+	code = 0;
+	ft_putstr_fd("exit\n", STDERR);
+	if (cmd_lst->args && cmd_lst->args[1] && !ft_isnum(cmd_lst->args[1]))
+	{
+		code = 255;
+		ft_putstr_fd("msh: exit: ", STDERR);
+		ft_putstr_fd(cmd_lst->args[1], STDERR);
+		ft_putendl_fd(": numeric argument required", STDERR);
+	}
+	else if (cmd_lst->args && cmd_lst->args[1] && cmd_lst->args[2])
+	{
+		code = 1;
+		ft_putstr_fd("msh: exit: too many arguments\n", STDERR);
+	}
+	else if (cmd_lst->args && cmd_lst->args[1])
+		code = ft_atoi(cmd_lst->args[1]);
+	free_exit(status, env_lst, cmd_lst);
 	printf("========> Exit Status: %d\n", code);
 	exit(code);
 	return (code);
