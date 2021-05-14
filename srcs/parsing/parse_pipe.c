7b88744bdcd665551fa3936b2234a504e3874773
Jonny Saykosy <josayko@pm.me>
2021-05-14 11:11:48 +0200

    update parse_pipe.c to handle empty args

--- srcs/parsing/parse_pipe.c
+++ srcs/parsing/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 12:32:40 by jonny             #+#    #+#             */
-/*   Updated: 2021/05/04 17:00:55 by jonny            ###   ########.fr       */
+/*   Updated: 2021/05/14 11:07:26 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,28 @@ bool		check_pipe(t_cmd *cmd_lst)
 	return (false);
 }
 
-static void	init_piped_cmd(enum e_type *types, t_cmd **piped_cmd, char *buffer)
-{
-	t_cmd	*new_cmd;
-	int		i;
+// static void	init_piped_cmd(enum e_type *types, t_cmd **piped_cmd, char *buffer)
+// {
+// 	t_cmd	*new_cmd;
+// 	int		i;
 
-	new_cmd = ft_calloc(1, sizeof(t_cmd));
-	i = 0;
-	while (types[i] != PIPE && types[i] != VOID)
-	{
-		new_cmd->type[i] = types[i];
-		i++;
-	}
-	new_cmd->type[i] = VOID;
-	new_cmd->args = split_whitespace(buffer);
-	new_cmd->next = NULL;
-	cmd_lst_add(piped_cmd, new_cmd);
-}
+// 	new_cmd = ft_calloc(1, sizeof(t_cmd));
+// 	i = 0;
+// 	while (types[i] != PIPE && types[i] != VOID)
+// 	{
+// 		new_cmd->type[i] = types[i];
+// 		i++;
+// 	}
+// 	new_cmd->type[i] = VOID;
+// 	new_cmd->args = split_whitespace(buffer);
+// 	new_cmd->next = NULL;
+// 	cmd_lst_add(piped_cmd, new_cmd);
+// }
 
 void		reset_buf(char *buffer, enum e_type *types)
 {
-	ft_bzero(buffer, BUF_SIZE);
+	(void)buffer;
+	// ft_bzero(buffer, BUF_SIZE);
 	ft_bzero(types, BUF_SIZE);
 }
 
@@ -58,29 +59,56 @@ void		concat_buf(char *buffer, char *str)
 
 void		parse_pipe(int i, int j, t_cmd *cmd_lst, t_cmd **piped_cmd)
 {
-	char		buffer[BUF_SIZE];
 	enum e_type	types[BUF_SIZE];
+	(void)piped_cmd;
+	t_cmd	*new_cmd;
+	int		index;
+	int		size;
 
 	i = 0;
 	j = 0;
-	reset_buf(buffer, types);
+	index = 0;
+	reset_buf(NULL, types);
 	while (1)
 	{
 		if (cmd_lst->type[i] == PIPE || cmd_lst->type[i] == VOID)
 		{
-			types[j] = cmd_lst->type[i];
-			init_piped_cmd(types, piped_cmd, buffer);
-			if (cmd_lst->type[i] == VOID)
-				break ;
-			reset_buf(buffer, types);
+			size = i - index + 1;
+			new_cmd = ft_calloc(1, sizeof(t_cmd));
+			new_cmd->next = NULL;
+			new_cmd->args = ft_calloc(size, sizeof(char *));
 			j = 0;
+			while (index < i)
+			{
+				new_cmd->args[j] = ft_strdup(cmd_lst->args[index]);
+				index++;
+				j++;
+			}
+			index = i + 1;
+			cmd_lst_add(piped_cmd, new_cmd);
 		}
-		else
-		{
-			types[j] = cmd_lst->type[i];
-			concat_buf(buffer, cmd_lst->args[i]);
-			j++;
-		}
+		if (cmd_lst->type[i] == VOID)
+			break ;
 		i++;
 	}
+
+	// while (1)
+	// {
+		// if (cmd_lst->type[i] == PIPE || cmd_lst->type[i] == VOID)
+		// {
+			// types[j] = cmd_lst->type[i];
+			// init_piped_cmd(types, piped_cmd, buffer);
+			// if (cmd_lst->type[i] == VOID)
+				// break ;
+			// reset_buf(buffer, types);
+			// j = 0;
+		// }
+		// else
+		// {
+			// types[j] = cmd_lst->type[i];
+			// concat_buf(buffer, cmd_lst->args[i]);
+			// j++;
+		// }
+		// i++;
+	// }
 }
