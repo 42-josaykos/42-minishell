ca4cb735c2ea53330fc9a16c780cd92bf8642892
Jonny Saykosy <josayko@pm.me>
2021-06-01 16:05:40 +0200

    minor update in handle_dbl_quotes()

--- srcs/parsing/handle_dbl_quotes.c
+++ srcs/parsing/handle_dbl_quotes.c
@@ -6,7 +6,7 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 13:33:32 by jonny             #+#    #+#             */
-/*   Updated: 2021/06/01 15:05:17 by jonny            ###   ########.fr       */
+/*   Updated: 2021/06/01 16:04:48 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	handle_dbl_quotes(t_ast **token, char *buf, t_env *env_lst)
 		|| (g_sig.dollar_quote && (*token)->type == QUEST))
 		handle_variables_quotes(buf, token, env_lst);
 	else if ((*token)->type == ESCAPE && (*token)->right
-		&& (*token)->right->value[0] == '\'')
+		&& ((*token)->right->value[0] == '\''
+			|| (*token)->right->value[0] == ' '))
 		ft_strcat(buf, (*token)->value);
 	else if ((*token)->type != DOLLAR
 		&& (g_sig.dollar_quote || (*token)->type != ESCAPE)
